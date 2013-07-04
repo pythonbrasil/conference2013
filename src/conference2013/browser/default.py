bf7c1a342442733ae1fef5eda40c0d087cf9a258
Érico Andrei <erico@simplesconsultoria.com.br>
2013-07-04 19:43:34 -0300

    Add default redirector

---
+++ src/conference2013/browser/default.py
@@ -0,0 +1,33 @@
+# -*- coding:utf-8 -*-
+from five import grok
+from Products.CMFPlone.interfaces import IPloneSiteRoot
+
+
+class DefaultPageView(grok.View):
+    grok.context(IPloneSiteRoot)
+    grok.require('zope2.View')
+    grok.name('front-page')
+
+    def get_hostname(self, request):
+        if "HTTP_X_FORWARDED_HOST" in request.environ:
+            # Virtual host
+            host = request.environ["HTTP_X_FORWARDED_HOST"]
+        elif "HTTP_HOST" in request.environ:
+            # Direct client request
+            host = request.environ["HTTP_HOST"]
+        else:
+            host = ''
+        return host
+
+    def render(self):
+        request = self.request
+        hostname = self.get_hostname(request)
+        if 'ploneconf' in hostname:
+            path = 'ploneconf'
+        else:
+            path = 'pythonbrasil'
+        context = self.context
+        portal_state = context.restrictedTraverse('@@plone_portal_state')
+        return request.RESPONSE.redirect(
+            portal_state.portal_url() + '/' + path
+        )
